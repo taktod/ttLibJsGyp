d861f88a0b5996a0ed02dd8028b5e0761aab54d8
taktod <poepoemix@hotmail.com>
2018-11-21 13:52:16 +0900

    ttLibCの内容を更新してyuvj444pの内容が正しく処理できるようにした。

---
+++ test/imageCheckTest.js
@@ -0,0 +1,50 @@
+/**
+ * 画像データをいじる動作テスト
+ */
+var tt = require("../");
+var fs = require("fs");
+var assert = require("assert");
+
+describe("imageCheck", () => {
+  it("jpeg", (done) => {
+    // binaryからpngデータを復元するテスト処理
+    var frame = new tt.Frame();
+    frame.binary = fs.readFileSync("./target.jpeg");
+    frame.type = "jpeg";
+    frame.timebase = 1000;
+    frame.pts = 0;
+    frame.restore();
+    var decoder = new tt.decoder.AvcodecVideoDecoder(frame.type, frame.width, frame.height);
+    decoder.decode(frame, (frame) => {
+      var encoder = new tt.encoder.JpegEncoder(frame.width, frame.height, 90);
+      return encoder.encode(frame, (frame) => {
+        fs.writeFileSync("output_jpeg.jpeg", frame.getBinaryBuffer());
+        done();
+        return true;
+      });
+    });
+  });
+  it("png", (done) => {
+    var frame = new tt.Frame();
+    frame.binary = fs.readFileSync("./target.png");
+    frame.type = "png";
+    frame.timebase = 1000;
+    frame.pts = 0;
+    frame.restore();
+    var decoder = new tt.decoder.AvcodecVideoDecoder(frame.type, frame.width, frame.height);
+    decoder.decode(frame, (frame) => {
+      var resampler = new tt.resampler.SwscaleResampler(
+        frame.type, frame.subType, frame.width, frame.height,
+        "yuv", "planar", frame.width, frame.height,
+        "bilinear");
+      return resampler.resample(frame, (frame) => {
+        var encoder = new tt.encoder.JpegEncoder(frame.width, frame.height, 90);
+        return encoder.encode(frame, (frame) => {
+          fs.writeFileSync("output_png.jpeg", frame.getBinaryBuffer());
+          done();
+          return true;
+        });
+      });
+    });
+  });
+});
